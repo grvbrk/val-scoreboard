ec253696e9a8602ffdc7cabc5da1bf62f8d6135a
grvbrk <gouravbarikofficial@gmail.com>
2025-03-15 18:33:58 +0530

    added upcoming match scrapers

---
+++ python-scraper/src/scrapers/matches/upcoming/all_matches.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_upcoming_matches():
+    url = "https://www.vlr.gg"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    }
+
+    results = []
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return {"data": {"status": response.status_code, "segments": results}}
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    matches = soup.select(".js-home-matches-upcoming a.wf-module-item")
+    for match in matches:
+        is_upcoming = match.select_one(".h-match-eta.mod-upcoming")
+        if is_upcoming:
+
+            teams = []
+            for team in match.select(".h-match-team"):
+                teams_el = team.select_one(".h-match-team-name")
+                teams.append(teams_el.getText().strip())
+
+                # flag_el = team.select_one(".flag")
+                # flags.append(
+                #     "".join(flag_el.get("class"))
+                #     .replace("mod-16", "")
+                #     .replace("mod", "")
+                #     if flag_el
+                #     else "N/A"
+                # )
+
+                # score_el = team.select_one(".h-match-team-score")
+                # scores.append(score_el.getText().strip() if score_el else "N/A")
+
+            # eta_el = match.select_one(".h-match-eta")
+            # eta = eta_el.getText().strip() if eta_el else "TBD"
+            # if eta != "LIVE":
+            #     eta += " from now"
+
+            # match_event_elem = match.select_one(".h-match-preview-event")
+            # match_event = (
+            #     match_event_elem.getText().strip() if match_event_elem else "Unknown"
+            # )
+
+            # match_series_elem = match.select_one(".h-match-preview-series")
+            # match_series = (
+            #     match_series_elem.getText().strip() if match_series_elem else "Unknown"
+            # )
+
+            # timestamp_elem = match.select_one(".moment-tz-convert")
+            # timestamp = (
+            #     datetime.fromtimestamp(
+            #         int(timestamp_elem["data-utc-ts"]), tz=timezone.utc
+            #     ).strftime("%Y-%m-%d %H:%M:%S")
+            #     if timestamp_elem
+            #     else "Unknown"
+            # )
+
+            url_path = f"https://www.vlr.gg/{match.get("href")}"
+            # response = requests.get(url_path, headers=headers)
+            # if response.status_code != 200:
+            #     return {"data": {"status": response.status_code, "segments": results}}
+
+            # match_soup = BeautifulSoup(response.text, "html.parser")
+            # team_shorts = match_soup.select("div.ge-text-light")
+            # print(team_shorts)
+            # team1_short = team_shorts[0].getText().strip()
+            # team2_short = team_shorts[5].getText().strip()
+
+            results.append(
+                {
+                    "team1": teams[0] if len(teams) > 0 else "Unknown",
+                    "team2": teams[1] if len(teams) > 1 else "Unknown",
+                    # "flag1": flags[0] if len(flags) > 0 else "Unknown",
+                    # "flag2": flags[1] if len(flags) > 1 else "Unknown",
+                    # "team1_short": team1_short,
+                    # "team2_short": team2_short,
+                    # "time_until_match": eta,
+                    # "match_series": match_series,
+                    # "match_event": match_event,
+                    # "unix_timestamp": timestamp,
+                    "match_page": url_path,
+                }
+            )
+
+    return {"data": {"status": response.status_code, "segments": results}}
---
+++ python-scraper/src/scrapers/matches/upcoming/single_match.py
@@ -0,0 +1,116 @@
+import requests
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+
+def get_ordinal(n):
+    return (
+        f"{n}{'th' if 11 <= n <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')}"
+    )
+
+
+def scrape_single_upcoming_match(url: str):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    }
+
+    results = []
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return {"data": {"status": response.status_code, "segments": results}}
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    match_info = soup.select_one("div.match-header")
+
+    match_series = (
+        match_info.select_one("a.match-header-event > div > div:nth-of-type(1)")
+        .getText()
+        .strip()
+    )
+    match_event = (
+        match_info.select_one("a.match-header-event div.match-header-event-series")
+        .getText()
+        .strip()
+        .replace("\n", "")
+        .replace("\t", "")
+    )
+
+    utc_string = match_info.select_one(
+        "div.match-header-date > div:nth-of-type(2)"
+    ).get("data-utc-ts")
+
+    unix_timestamp = (
+        datetime.strptime(utc_string, "%Y-%m-%d %H:%M:%S") + timedelta(hours=4)
+    ).strftime("%Y-%m-%d %H:%M:%S")
+
+    match_time = (
+        datetime.strptime(utc_string, "%Y-%m-%d %H:%M:%S") + timedelta(hours=4)
+    ).strftime("%-I:%M %p UTC")
+
+    match_date = (
+        match_info.select_one("div.match-header-date > div:nth-of-type(1)")
+        .getText()
+        .strip()
+    )
+
+    match_series_logo = match_info.select_one("a.match-header-event img").get("src")
+    match_total_rounds = (
+        match_info.select_one("div.match-header-vs-score > div:nth-of-type(3)")
+        .getText()
+        .strip()
+    )
+
+    teams = match_info.select(
+        ".match-header-link .match-header-link-name .wf-title-med"
+    )
+    team1 = teams[0].getText().strip()
+    team2 = teams[1].getText().strip()
+
+    team_logos = match_info.select(".match-header-link img")
+    team1_logo = team_logos[0].get("src")
+    team2_logo = team_logos[1].get("src")
+
+    # _________________________________________ #
+
+    players_info = soup.select("div.vm-stats-game")[0].select("table")
+    team1_players = players_info[0].select("tbody tr")
+    team2_players = players_info[1].select("tbody tr")
+
+    team1_short = team1_players[0].select_one(".ge-text-light").getText().strip()
+    team2_short = team2_players[0].select_one(".ge-text-light").getText().strip()
+
+    players1 = []
+    players2 = []
+
+    for player in team1_players:
+        name = player.select_one(".text-of").getText().strip()
+        player_flag = "".join(player.select_one("i").get("class")).replace("mod", "")
+        players1.append({"name": name, "flag": player_flag})
+
+    for player in team2_players:
+        name = player.select_one(".text-of").getText().strip()
+        player_flag = "".join(player.select_one("i").get("class")).replace("mod", "")
+        players2.append({"name": name, "flag": player_flag})
+
+    results.append(
+        {
+            "team1": team1,
+            "team2": team2,
+            "logo1": team1_logo,
+            "logo2": team2_logo,
+            "team1_short": team1_short,
+            "team2_short": team2_short,
+            "players1": players1,
+            "players2": players2,
+            "match_series": match_series,
+            "match_event": match_event,
+            "event_logo": match_series_logo,
+            "match_date": match_date,
+            "match_time": match_time,
+            "unix_timestamp": unix_timestamp,
+            "rounds": match_total_rounds,
+        }
+    )
+    return {"data": {"status": response.status_code, "segments": results}}
