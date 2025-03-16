e7acf12c7a64e994746d0bc86a76a75a10b61c8e
grvbrk <gouravbarikofficial@gmail.com>
2025-03-16 17:42:41 +0530

    router done

--- python-scraper/src/routers/matches.py
+++ python-scraper/src/routers/matches.py
@@ -1,26 +1,68 @@
-from fastapi import APIRouter
-from src.scrapers.matches.match_scraper import (
-    scrape_upcoming_matches,
-    scrape_live_matches,
-    scrape_all_match_results,
-)
+from fastapi import APIRouter, Query
+from typing import Annotated
+from src.scrapers.matches.upcoming.all_matches import scrape_upcoming_matches
+from src.scrapers.matches.upcoming.single_match import scrape_single_upcoming_match
+from src.scrapers.matches.live.single_match import scrape_single_live_match
+from src.scrapers.matches.results.all_matches import scrape_all_match_results
+from src.scrapers.matches.results.single_match import scrape_single_match_result
 from src.models.live_model import LiveMatchResponse
-from src.models.upcoming_model import UpcomingMatchResponse
-from src.models.results_model import MatchResultsResponse
+from src.models.upcoming_model import (
+    AllUpcomingMatchesResponse,
+    SingleUpcomingMatchResponse,
+)
+from src.models.results_model import AllMatchResultsResponse, SingleMatchResultResponse
+
 
 router = APIRouter()
 
 
-@router.get("/match/upcoming")
-async def get_matches() -> UpcomingMatchResponse:
+@router.get("/match/upcoming/all")
+async def get_matches() -> AllUpcomingMatchesResponse:
     return scrape_upcoming_matches()
 
 
+@router.get("/match/upcoming/single")
+async def get_matches(
+    url: Annotated[
+        str | None,
+        Query(
+            title="match url",
+            description="vlr.gg match url for scraping upcoming match data",
+            regex=r"^https:\/\/www\.vlr\.gg\/\d+\/[\w-]+\/?$",
+        ),
+    ] = None,
+) -> SingleUpcomingMatchResponse:
+    return scrape_single_upcoming_match(url)
+
+
 @router.get("/match/live")
-async def get_matches() -> LiveMatchResponse:
-    return scrape_live_matches()
+async def get_matches(
+    url: Annotated[
+        str | None,
+        Query(
+            title="match url",
+            description="vlr.gg match url for scraping live match data",
+            regex=r"^https:\/\/www\.vlr\.gg\/\d+\/[\w-]+\/?$",
+        ),
+    ] = None,
+) -> LiveMatchResponse:
+    return scrape_single_live_match(url)
 
 
-@router.get("/match/results")
-async def get_matches() -> MatchResultsResponse:
+@router.get("/match/results/all")
+async def get_matches() -> AllMatchResultsResponse:
     return scrape_all_match_results()
+
+
+@router.get("/match/results/single")
+async def get_matches(
+    url: Annotated[
+        str | None,
+        Query(
+            title="match url",
+            description="vlr.gg match url for scraping match results",
+            regex=r"^https:\/\/www\.vlr\.gg\/\d+\/[\w-]+\/?$",
+        ),
+    ] = None,
+) -> SingleMatchResultResponse:
+    return scrape_single_match_result(url)
