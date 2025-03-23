54d063bb89b08309b0702d6cb937d66259faa5d5
grvbrk <gouravbarikofficial@gmail.com>
2025-03-23 18:34:43 +0530

    upcoming, live and result post preview components

---
+++ src/components/LivePreview.tsx
@@ -0,0 +1,65 @@
+import { Devvit } from '@devvit/public-api';
+import { CLR_DUTCH_WHITE } from 'src/core/colors.js';
+import { AllUpcomingMatchSegment } from 'src/core/types.js';
+import { MAPS } from 'src/utils/maps.js';
+
+export const LivePreview: Devvit.BlockComponent<{
+  upcomingMatchInfo: AllUpcomingMatchSegment;
+}> = ({ upcomingMatchInfo }) => {
+  return (
+    <zstack height="100%" width="100%">
+      <hstack height={100} width={100}>
+        {MAPS.map((map) => {
+          return (
+            <image
+              url={`maps/${map}.png`}
+              imageHeight={100}
+              imageWidth={100}
+              height={100}
+              width={100}
+              resizeMode="cover"
+              grow
+            />
+          );
+        })}
+      </hstack>
+      <vstack height="100%" width="100%" alignment="middle center">
+        <text size="xlarge" weight="bold" color={CLR_DUTCH_WHITE} wrap>
+          {upcomingMatchInfo.match_event}
+        </text>
+        <text size="small" color={CLR_DUTCH_WHITE}>
+          {upcomingMatchInfo.match_series} • Live
+        </text>
+
+        <spacer size="medium" />
+        <hstack alignment="center middle" width={100}>
+          <hstack width={50} alignment="end middle">
+            <image
+              url={`flags/${upcomingMatchInfo.flag1.replace('flag-', '')}.png`}
+              imageHeight={16}
+              imageWidth={16}
+            />
+            <spacer size="xsmall" />
+            <text alignment="end middle" color={CLR_DUTCH_WHITE} size="large" weight="bold" wrap>
+              {upcomingMatchInfo.team1}
+            </text>
+          </hstack>
+          <spacer size="large" />
+          <text color={CLR_DUTCH_WHITE}>vs</text>
+          <spacer size="large" />
+          <hstack width={50} alignment="start middle">
+            <image
+              url={`flags/${upcomingMatchInfo.flag2.replace('flag-', '')}.png`}
+              imageHeight={16}
+              imageWidth={16}
+            />
+            <spacer size="xsmall" />
+            <text alignment="start middle" color={CLR_DUTCH_WHITE} size="large" weight="bold" wrap>
+              {upcomingMatchInfo.team2}
+            </text>
+          </hstack>
+        </hstack>
+      </vstack>
+    </zstack>
+  );
+};
