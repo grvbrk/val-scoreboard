3e7496c53933b826d6e125826fd09d29040f79b0
grvbrk <gouravbarikofficial@gmail.com>
2025-03-27 22:28:25 +0530

    webview utils

--- game/shared.ts
+++ game/shared.ts
@@ -1,23 +1,29 @@
-export type Page =
-  | "home"
-  | "pokemon";
+import { PredictionType, SingleUpcomingMatchSegment } from '../src/core/types';
 
-export type WebviewToBlockMessage = { type: "INIT" } | {
-  type: "GET_POKEMON_REQUEST";
-  payload: { name: string };
-};
+export type Page = 'prediction' | 'results';
 
-export type BlocksToWebviewMessage = {
-  type: "INIT_RESPONSE";
-  payload: {
-    postId: string;
-  };
-} | {
-  type: "GET_POKEMON_RESPONSE";
-  payload: { number: number; name: string; error?: string };
-};
+export type WebviewToBlockMessage =
+  | { type: 'INIT' }
+  | {
+      type: 'SEND_USER_PREDS';
+      payload: PredictionType;
+    };
+
+export type BlocksToWebviewMessage =
+  | {
+      type: 'INIT_RESPONSE';
+      payload: {
+        postId: string;
+        matchData: SingleUpcomingMatchSegment;
+        userPreds: PredictionType | null;
+      };
+    }
+  | {
+      type: 'GET_POKEMON_RESPONSE';
+      payload: { number: number; name: string; error?: string };
+    };
 
 export type DevvitMessage = {
-  type: "devvit-message";
+  type: 'devvit-message';
   data: { message: BlocksToWebviewMessage };
 };
